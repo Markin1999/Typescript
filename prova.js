242e69e4e110ac6a231c6377bf88d62c0e0efdf0
Marco Filannino <filanninobusiness@gmail.com>
2025-01-22 14:00:53 +0100

    Aggiunti esercizi primitive types

--- prova.js
+++ prova.js
@@ -1,22 +1,54 @@
-var numero = 23;
-var stringa = "marco";
-var booleano = true;
-var array = [1, 2, 3, 4];
-var arrayvs = ["marco", "luca", "giuseppe"];
-var tupla = ["Marco", 45, false];
-function somma(a, b) {
-    if (b === void 0) { b = 3; }
-    return a + b;
+// Aggiungere tipi a questa dichiarazione di funzione.
+function doubleThePopulation(value) {
+    return value * 2;
 }
-console.log(somma(numero));
-array.sort(function (a, b) { return b - a; });
-console.log(array);
-var nuovoArray = array.map(function (a) {
-    if (a === 2) {
-        return Number(a);
+// Correggere o rimuovere le chiamate di funzione che causano errori di tipo.
+doubleThePopulation(5);
+//doubleThePopulation("6.12");
+doubleThePopulation(8526);
+//doubleThePopulation(false);
+// Modifica questa funzione in modo che il language2parametro sia facoltativo.
+// Suggerimento: language2non controllare undefinedprima di passarlo a console.log().
+function languagesSpoken(country, language1, language2) {
+    console.log("The languages spoken in ".concat(country, " are:"));
+    console.log(language1);
+    if (language2) {
+        console.log(language2);
     }
-    else {
-        return String(a);
+}
+languagesSpoken("Colombia", "Spanish", "English");
+languagesSpoken("Greece", "Greek");
+languagesSpoken("New Zealand", "English", "Māori");
+//---
+function getPopulation() {
+    return 69950850;
+}
+// Aggiungere un'asserzione di tipo dopo la chiamata a getPopulation().
+// Suggerimento: qual è il tipo effettivo del valore restituito da getPopulation()?
+var population = getPopulation();
+// Ora che hai aggiunto un'asserzione di tipo, c'è un errore di tipo nel codice sottostante.
+// Rimuovi il codice non corretto.
+//console.log(population.toUpperCase());
+console.log(population * 2);
+var country1 = "Nigeria";
+var country2 = "Italy";
+var country3 = "China";
+// ----
+// Aggiungere il anytipo per correggere gli errori di tipo nel codice seguente.
+var currency = { name: "Indian rupee" };
+currency.code = "THB";
+currency = "Baht";
+// Correggi l'istruzione if in questa funzione in modo che il tipo di valuevenga ridotto a string.
+function countryNameLength(value) {
+    if (typeof value === "string") {
+        console.log(value.length);
     }
-});
-console.log(nuovoArray);
+}
+countryNameLength("United States of America");
+// Correggi l'errore di tipo che vediamo quando chiamiamo questa funzione.
+// Suggerimento: sostituisci uno dei nevertipi con un tipo diverso.
+function throwCountryError(message) {
+    throw new Error("Could not find country: ".concat(message));
+}
+throwCountryError("Narnia");
+// ----
--- prova.ts
+++ prova.ts
@@ -1,26 +1,91 @@
-let numero: number = 23;
-let stringa: string = "marco";
-let booleano: boolean = true;
-let array: number[] = [1, 2, 3, 4];
-let arrayvs: Array<string> = ["marco", "luca", "giuseppe"];
-let tupla: [string, number, boolean] = ["Marco", 45, false];
+// Aggiungere tipi a questa dichiarazione di funzione.
 
-function somma(a: number, b: number = 3): number {
-  return a + b;
+function doubleThePopulation(value: number): number {
+  return value * 2;
+}
+// Correggere o rimuovere le chiamate di funzione che causano errori di tipo.
+
+doubleThePopulation(5);
+
+//doubleThePopulation("6.12");
+
+doubleThePopulation(8_526);
+
+//doubleThePopulation(false);
+// Modifica questa funzione in modo che il language2parametro sia facoltativo.
+// Suggerimento: language2non controllare undefinedprima di passarlo a console.log().
+
+function languagesSpoken(
+  country: string,
+  language1: string,
+  language2?: string
+): void {
+  console.log(`The languages spoken in ${country} are:`);
+
+  console.log(language1);
+
+  if (language2) {
+    console.log(language2);
+  }
 }
 
-console.log(somma(numero));
+languagesSpoken("Colombia", "Spanish", "English");
 
-array.sort((a, b) => b - a);
+languagesSpoken("Greece", "Greek");
 
-console.log(array);
+languagesSpoken("New Zealand", "English", "Māori");
 
-let nuovoArray = array.map((a) => {
-  if (a === 2) {
-    return Number(a);
-  } else {
-    return String(a);
+//---
+
+function getPopulation(): number {
+  return 69_950_850;
+}
+// Aggiungere un'asserzione di tipo dopo la chiamata a getPopulation().
+// Suggerimento: qual è il tipo effettivo del valore restituito da getPopulation()?
+
+const population = getPopulation();
+
+// Ora che hai aggiunto un'asserzione di tipo, c'è un errore di tipo nel codice sottostante.
+// Rimuovi il codice non corretto.
+
+//console.log(population.toUpperCase());
+
+console.log(population * 2);
+// ----
+// Crea un Countryalias di tipo per il stringtipo.
+// Modifica il tipo per ogni variabile per utilizzare il Countrytipo.
+
+type Nazione = string;
+
+const country1: Nazione = "Nigeria";
+
+const country2: Nazione = "Italy";
+
+const country3: Nazione = "China";
+// ----
+
+// Aggiungere il anytipo per correggere gli errori di tipo nel codice seguente.
+
+let currency: any = { name: "Indian rupee" };
+
+currency.code = "THB";
+
+currency = "Baht";
+// Correggi l'istruzione if in questa funzione in modo che il tipo di valuevenga ridotto a string.
+
+function countryNameLength(value: unknown) {
+  if (typeof value === "string") {
+    console.log(value.length);
   }
-});
+}
 
-console.log(nuovoArray);
+countryNameLength("United States of America");
+// Correggi l'errore di tipo che vediamo quando chiamiamo questa funzione.
+// Suggerimento: sostituisci uno dei nevertipi con un tipo diverso.
+
+function throwCountryError(message: string): never {
+  throw new Error(`Could not find country: ${message}`);
+}
+
+throwCountryError("Narnia");
+// ----
